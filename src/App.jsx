51a7d24c66a570cf0e1780a1d51404a1b50f8fe6
piarda <matt.rindini@gmail.com>
2025-08-13 14:26:11 -0400

    updated code

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
+import About from './pages/About'
+import Login from './pages/Login'
+import UserProfile from './pages/UserProfile'
+import ErrorPage from './pages/ErrorPage'
 
 const App = () => {
     return (
-        <Home />
+        <BrowserRouter>
+            <Routes>
+                <Route path='*' element={<ErrorPage />} />
+                <Route path='/' element={<Home />} />
+                <Route path='/about' element={<About />} />
+                <Route path='/login' element={<Login />} />
+                <Route path='/profile/:id' element={<UserProfile />} />
+            </Routes>
+        </BrowserRouter>
     )
 }
 
